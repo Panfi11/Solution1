8416d823ca3ad424c1fee7d94beff64dfbe69964
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:21:58 +0200

    Add project files.

---
+++ ImportoScontato/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ImportoScontato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("imetti l'importo");
+            double totale = double.Parse(Console.ReadLine());
+            double sconto = totale * 100 / 20;
+            if (totale > 100)
+            {
+                double importo = totale - sconto;
+                Console.WriteLine($"l'importo scontato è{importo}");
+
+            }
+            else
+                Console.WriteLine($"la spesa è di {totale}");
+            Console.ReadLine();
+        } 
+    }
+}
